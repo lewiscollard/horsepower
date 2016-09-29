35c6d42c997feb4840c9fc4c60ba79bb9ca4be75
Lewis Collard <lewis.collard@onespacemedia.com>
2016-09-29 17:45:52 +0100

    Autplay carousel.

--- assets/js/50-carousel.js
+++ assets/js/50-carousel.js
@@ -4,7 +4,7 @@ function makeCarousel(selector) {
         return;
     }
     carousel.classList.add('carousel-loading');
-    lory(carousel, {
+    var loryCarousel = lory(carousel, {
         'infinite': 1,
         'classNameFrame': 'carousel-frame',
         'classNamePrevCtrl': 'carousel-button-previous',
@@ -18,4 +18,17 @@ function makeCarousel(selector) {
     window.addEventListener("load", function () {
         carousel.classList.remove('carousel-loading');
     })
+
+    // Autoplay
+    var timer = window.setInterval(function () {
+        loryCarousel.next();
+    }, 5000);
+
+    function cancelTimer() {
+        window.clearInterval(timer)
+    }
+
+    carousel.querySelector('.carousel-button-next').addEventListener('click', cancelTimer);
+    carousel.querySelector('.carousel-button-previous').addEventListener('click', cancelTimer);
+    carousel.addEventListener('on.lory.touchstart', cancelTimer);
 }
